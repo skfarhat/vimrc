341e3acd088aeafe8c7d56aad467b3a50ee2456a
Sami Farhat <samikfarhat@gmail.com>
2017-12-02 12:39:38 +0000

    add install script

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+me="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# install neobundle
+$me/scripts/install-neobundle.sh
+
+# create link ~/.vimrc
+pushd ~
+ln -s $me/vimrc .vimrc
+popd
